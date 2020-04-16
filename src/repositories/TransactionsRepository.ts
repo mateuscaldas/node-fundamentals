81c42c06cf73491662fc20f6607ba7d7415cf413
Mateus Queiroz <mateus.caldas@hotmail.com>
2020-04-16 20:08:58 +0100

    first commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +15,59 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes: number[] = [];
+    const outcomes: number[] = [];
+
+    this.transactions.map(income => {
+      if (income.type === 'income') {
+        incomes.push(income.value);
+      }
+      return null;
+    });
+
+    this.transactions.map(income => {
+      if (income.type === 'outcome') {
+        outcomes.push(income.value);
+      }
+      return null;
+    });
+
+    const totalIncome = incomes.reduce((acc, curr) => acc + curr, 0);
+    const totalOutcome = outcomes.reduce((acc, curr) => acc + curr, 0);
+
+    const total = totalIncome - totalOutcome;
+
+    this.balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
